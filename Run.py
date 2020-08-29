ac1002e5ae2276355f0570ae1303dd7133cce950
fernandafons <nanda.fonseca7@gmail.com>
2020-08-28 22:33:43 -0300

    organized project into 2 classes

--- Run.py
+++ Run.py
@@ -3,13 +3,21 @@ from Searcher import Searcher
 # ---------------
 
 # Set airports and dates for your trip below
-departure_airpts_togo = ['SAO', 'BSB']
-arrival_airports_togo = ['SEL']
-departure_airpts_back = ['SEL']
-arrival_airports_back = ['SAO', 'BSB']
-when_to_go = ['2021-05-01', '2021-05-02', '2021-05-03', '2021-05-04', '2021-05-05']
-when_to_return = ['2021-05-22', '2021-05-23', '2021-05-24', '2021-05-25', '2021-05-26']
+departure_airpts_togo = input("De quais aeroportos você deseja partir? ")
+arrival_airports_togo = input("Em quais aeroportos você deseja chegar? ")
+departure_airpts_back = input("De quais aeroportos você deseja partir na volta? ")
+arrival_airports_back = input("Em quais aeroportos você deseja chegar na volta? ")
+when_to_go = input("Quais as datas de ida? ")
+when_to_return = input("Quais as datas de volta? ")
+
+# -----------------------------------
+departure_airpts_togo = departure_airpts_togo.split()
+arrival_airports_togo = arrival_airports_togo.split()
+departure_airpts_back = departure_airpts_back.split()
+arrival_airports_back = arrival_airports_back.split()
+when_to_go = when_to_go.split()
+when_to_return = when_to_return.split()
 
 searcher = Searcher(departure_airpts_togo, arrival_airports_togo, departure_airpts_back, 
-    arrival_airports_back, when_to_go, when_to_return)
-searcher.startSearching()
+                        arrival_airports_back, when_to_go, when_to_return)
+searcher.run()
--- Searcher.py
+++ Searcher.py
@@ -1,10 +1,8 @@
-
 from loguru import logger
 from Client import Client
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 # ----------------------------------------------
-
 
 class Searcher():
     def __init__(self, departure_airpts_togo, arrival_airports_togo, departure_airpts_back, 
@@ -20,8 +18,13 @@ class Searcher():
         self.precos = []
 
 
-    def startSearching(self):
+    def run(self):
+        self.__createUrlsList()
+        self.__searchPrices()
+        self.__sorter()
 
+
+    def __createUrlsList(self):
         for dep_airp_togo in self.departure_airpts_togo:
             for arr_airp_togo in self.arrival_airports_togo:
                 for date_togo in self.when_to_go:
@@ -29,28 +32,28 @@ class Searcher():
                         for arr_airp_back in self.arrival_airports_back:
                             for date_back in self.when_to_return:
                                 link_for_research = f"""https://www.kayak.com.br/flights/{dep_airp_togo}-{arr_airp_togo}/{date_togo}-h/{dep_airp_back}-{arr_airp_back}/{date_back}-h?sort=bestflight_a"""
-
                                 self.all_searches.append(link_for_research)
-
         print(self.all_searches)
         print(len(self.all_searches))
 
+
+    def __searchPrices(self):
         for link in self.all_searches:
             logger.debug(f"link: {link}")
-            client_response = Client(link, self.onload)
-            # time.sleep(5)
+            client_response = Client(link, self.__onload)
 
         logger.warning(f"Urls e preços: {self.precos}")
 
+
+    def __sorter(self):
         just_price = []
         for k in self.precos:
             just_price.append(int(k[1]))
-
         just_price.sort()
         logger.warning(f"Preços ordenados do mais barato para o mais caro: {just_price}")
 
 
-    def onload(self, link, html):
+    def __onload(self, link, html):
         bs = BeautifulSoup(html, 'html.parser')
         try:
             price = bs.find('span', {'class': 'price-text'}).getText()
@@ -61,4 +64,4 @@ class Searcher():
                 logger.error(f"I wasn't able to split price: {price}, {e}")
         except Exception as e:
             logger.error(e)
-        logger.warning(f"precos dentro do onload: {self.precos}")
+        # logger.warning(f"precos dentro do onload: {self.precos}")
